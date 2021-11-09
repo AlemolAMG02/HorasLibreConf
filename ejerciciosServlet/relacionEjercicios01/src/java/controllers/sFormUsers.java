d42ca69127f5cb60ca9cb10f83c1dd5ac63a2617
AlemolAMG02 <amolgom25@iesmarquesdecomares.org>
2021-11-09 08:45:20 +0100

    UP repositorio

--- ejerciciosServlet/relacionEjercicios01/src/java/controllers/sFormUsers.java
+++ ejerciciosServlet/relacionEjercicios01/src/java/controllers/sFormUsers.java
@@ -2,25 +2,24 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-package controller;
+package controllers;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import modelo.ListaUsuarios;
 import modelo.Usuario;
 
 /**
  *
  * @author alemol
  */
-@WebServlet(name = "s1", urlPatterns = {"/s1"})
-public class s1 extends HttpServlet {
+@WebServlet(name = "sFormUsers", urlPatterns = {"/sFormUsers"})
+public class sFormUsers extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -34,24 +33,58 @@ public class s1 extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+        
         try (PrintWriter out = response.getWriter()) {
+            String nombre = request.getParameter("user");
+            String contra = request.getParameter("pass");
             
-            HttpSession miSesion = request.getSession();    //Recuperamos la sesión
-            String nombre=request.getParameter("nombre");
-            String pass = request.getParameter("pass");
+            ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
+            usuarios.add(new Usuario());
+            usuarios.add(new Usuario("Fran","1234"));
+            usuarios.add(new Usuario("Sabrina","1234"));
             
-            ListaUsuarios usuarios = (ListaUsuarios) request.getAttribute("usuarios");
-            if(usuarios != null){
-                Usuario admin = new Usuario();  //Por defecto creamos un usuario administrador
-                
-                if(nombre.equals(admin.getNombre()) 
-                        && pass.equals(admin.getPasswd())){
-                    miSesion.setAttribute("logueado", nombre);             
-                } else {
-                    miSesion.setAttribute("logueado", "usuario incorrecto");
-                    //miSesion.setAttribute("logueado", nombre);
+            boolean nomCorr = false, passCorr = false;
+            
+            for (Usuario user : usuarios) {
+                if(nombre.equals(user.getNombre())){
+                    nomCorr = true;
+                    if(contra.equals(user.getPass()))
+                        passCorr = true;        
                 }
-                response.sendRedirect("index.jsp");
+            }
+            
+            if(nomCorr && passCorr){
+                out.println("<!DOCTYPE html>");
+                out.println("<html>");
+                out.println("<head>");
+                out.println("<title>ComprobarPass</title>");            
+                out.println("</head>");
+                out.println("<body>");
+                out.println("<h1>El usuario " + nombre + " y su contraseña es correcta </h1>");
+                out.println("</body>");
+                out.println("</html>");
+                
+            } else {
+            
+                out.println("<!DOCTYPE html>");
+                out.println("<html>");
+                out.println("<head>");
+                out.println("<title>ComprobarPass</title>");            
+                out.println("</head>");
+                out.println("<body>");
+                out.println("<h1>Inicia sesión: </h1>");
+                out.println("<form action=\"sFormUsers\" method=\"post\">\n" +
+"            Usuario: <input type=\"text\" name=\"user\" placeholder=\"Usuario\">\n<br>" +
+"            Contraseña: <input type=\"password\" name=\"pass\" placeholder=\"Contraseña\">\n<br>" +
+"            <input type=\"submit\" name=\"enviar\" value=\"Comprobar\"/>\n" +
+"        </form>");
+                if(nomCorr){
+                    out.println("<p>Contraseña para " + nombre + " es incorrecta");
+                } else {
+                    out.println("<p>El usuario " + nombre + " no existe");
+                }
+                out.println("</body>");
+                out.println("</html>");
             }
         }
     }
