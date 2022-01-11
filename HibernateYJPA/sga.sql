7248294eab3483e2ad329f3068727bb9dc869223
Alemol <91252055+AlemolAMG02@users.noreply.github.com>
2022-01-11 10:43:18 +0100

    ADD nuevo ejercicio visto en clase

---
+++ HibernateYJPA/sga.sql
@@ -0,0 +1,82 @@
+-- MySQL dump 10.13  Distrib 5.7.24, for Linux (x86_64)
+--
+-- Host: localhost    Database: SGA
+-- ------------------------------------------------------
+-- Server version	5.7.24-0ubuntu0.18.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Persona`
+--
+create database if not exists sga;
+use sga;
+
+DROP TABLE IF EXISTS `Persona`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Persona` (
+  `id_persona` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `apellido` varchar(45) NOT NULL,
+  PRIMARY KEY (`id_persona`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Persona`
+--
+
+LOCK TABLES `Persona` WRITE;
+/*!40000 ALTER TABLE `Persona` DISABLE KEYS */;
+INSERT INTO `Persona` VALUES (10,'Juan','Martinez'),(11,'Ana','Lopez');
+/*!40000 ALTER TABLE `Persona` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuario` (
+  `id_usuario` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id_persona` int(10) unsigned DEFAULT NULL,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  PRIMARY KEY (`id_usuario`),
+  KEY `FK_id_persona` (`id_persona`),
+  CONSTRAINT `FK_id_persona` FOREIGN KEY (`id_persona`) REFERENCES `Persona` (`id_persona`) ON DELETE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (1,NULL,'lgomez','123'),(2,NULL,'evaldez','456'),(3,NULL,'jgutierrez','789'),(4,10,'jmartinez','123');
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-01-27 17:20:26
---
+++ ejerciciosServlet/PlantillaExamen/src/java/controllers/ejecuta.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p1;
+
+import com.mysql.jdbc.Driver;
+//import com.mysql.jdbc;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author alejandro
+ */
+public class ejecuta {
+
+    Connection conexion;
+    Statement instruccion;
+    ResultSet result;
+
+    public ejecuta(String sentenciasql) {
+
+        String url = "jdbc:mysql://localhost:3306/pruebaHLC?autoReconnect=true&useSSL=false";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            // Creamos el objeto conexion
+            this.conexion = (Connection) DriverManager.getConnection(url, "ale", "1234");
+            // Creamos un objeto Statement
+            this.instruccion = (Statement) conexion.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_UPDATABLE);
+            // Creamos el query
+            // String sql = "select id_persona, nombre, apellido from persona";
+            this.result = instruccion.executeQuery(sentenciasql);
+            // Cerrar cada uno de los objetos utilizados
+
+        } catch (ClassNotFoundException | SQLException e) {
+        }
+
+    }
+
+    public ResultSet getResult() {
+        return result;
+    }
+
+}
