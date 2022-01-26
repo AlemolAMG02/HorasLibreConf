20b59481784dc092ca04f8f043c4806aad1958ac
Alemol <91252055+AlemolAMG02@users.noreply.github.com>
2022-01-26 16:47:29 +0100

    UP Ejercicios corregidos y preparados

--- horasLibreConf.sql
+++ horasLibreConf.sql
@@ -21,8 +21,8 @@ SET time_zone = "+00:00";
 -- Base de datos: `pruebasHLC`
 --
 
-CREATE DATABASE IF NOT EXISTS pruebaHLC;
-USE pruebaHLC;
+/*CREATE DATABASE IF NOT EXISTS pruebaHLC;
+USE pruebaHLC; */
 -- --------------------------------------------------------
 
 --
