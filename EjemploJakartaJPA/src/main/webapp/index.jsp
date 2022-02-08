6bfc39c1eea32c7b03605cd5b7a42b45ee48dad8
Alemol <91252055+AlemolAMG02@users.noreply.github.com>
2022-02-08 10:37:40 +0100

    ADD Ejercicio de inicio de sprint boot

--- EjemploJakartaJPA/src/main/webapp/index.jsp
+++ EjemploJakartaJPA/src/main/webapp/index.jsp
@@ -59,11 +59,6 @@
                             <label for="pwd" class="form-label">Password:</label>
                             <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd">
                         </div>
-                        <div class="form-check mb-1">
-                            <label class="form-check-label">
-                                <input class="form-check-input" type="checkbox" name="remember"> Remember me
-                            </label>
-                        </div>
                         <button type="submit" class="btn btn-primary">Submit</button>
                     </form>
                 </div>
---
+++ airport.sql
@@ -0,0 +1,109 @@
+-- MySQL dump 10.13  Distrib 8.0.27, for Linux (x86_64)
+--
+-- Host: localhost    Database: airport
+-- ------------------------------------------------------
+-- Server version	8.0.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `avion`
+--
+drop database if exists airport;
+create database if not exists airport;
+use airport;
+
+DROP TABLE IF EXISTS `avion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `avion` (
+  `idav` int NOT NULL,
+  `compania` varchar(45) DEFAULT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `horasvuelo` int DEFAULT '0',
+  PRIMARY KEY (`idav`),
+  UNIQUE KEY `nombre_UNIQUE` (`nombre`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `avion`
+--
+
+LOCK TABLES `avion` WRITE;
+/*!40000 ALTER TABLE `avion` DISABLE KEYS */;
+INSERT INTO `avion` VALUES (1,'Air Lucena','Ciudad de Lucena',32),(2,'Air Lucena','Virgen de Araceli',7),(3,'Lucena Flying','Sierra de Aras',17),(4,'Lucena Flying','Tres Culturas',7),(5,'Air Lucena','xxxx',12),(10,'AirCabra','Sierrita',33);
+/*!40000 ALTER TABLE `avion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `destino`
+--
+
+DROP TABLE IF EXISTS `destino`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `destino` (
+  `iddest` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) DEFAULT NULL,
+  `tiempo` int DEFAULT '0',
+  PRIMARY KEY (`iddest`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `destino`
+--
+
+LOCK TABLES `destino` WRITE;
+/*!40000 ALTER TABLE `destino` DISABLE KEYS */;
+INSERT INTO `destino` VALUES (1,'Roma',4),(2,'Nueva York',12),(3,'Londres',3),(4,'Madrid',1),(5,'Málaga',1),(6,'París',2);
+/*!40000 ALTER TABLE `destino` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `vuelo`
+--
+
+DROP TABLE IF EXISTS `vuelo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `vuelo` (
+  `idvuelo` int NOT NULL AUTO_INCREMENT,
+  `idavion` int DEFAULT NULL,
+  `iddestino` int DEFAULT NULL,
+  PRIMARY KEY (`idvuelo`),
+  KEY `fk2_idx` (`iddestino`),
+  CONSTRAINT `fk2` FOREIGN KEY (`iddestino`) REFERENCES `destino` (`iddest`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `vuelo`
+--
+
+LOCK TABLES `vuelo` WRITE;
+/*!40000 ALTER TABLE `vuelo` DISABLE KEYS */;
+INSERT INTO `vuelo` VALUES (1,1,1);
+/*!40000 ALTER TABLE `vuelo` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-02-01  9:36:18
